4434927b468191766c750bd4ddc6a02e02f1ab34
Edgar Veiga <edgar.veiga@ndrive.com>
2011-12-28 00:21:05 +0000

    Add runserver as main process.

--- server.py
+++ server.py
@@ -4,11 +4,6 @@ import subprocess
 def start_supplier(host, port):
     pass
 
-def start_frontend(host, port):
-    subprocess.Popen(
-        ['python', 'frontend/manage.py', 'runserver', '%s:%s'%(host, port,)],
-    )
-
 def start_manufacturer(host, port):
     subprocess.Popen(['python', 'manufacturer/soap_services.py', host, port],)
 
@@ -17,13 +12,19 @@ def start_backoffice(host, port):
 
 if __name__=='__main__':
     try:
-        start_frontend("127.0.0.1", "8000")
         start_manufacturer("localhost","7889")
         start_manufacturer("localhost","7890")
-
         start_backoffice("localhost","7891")
     except Exception, e:
            print "Error: unable to start process %s"%(str(e),)
 
-    while True:
-        pass
+    # Start frontend and wait for calls
+    process = subprocess.Popen(
+        [
+            'python',
+            'frontend/manage.py',
+            'runserver',
+            '%s:%s'%("127.0.0.1", 8000,),
+        ],
+    )
+    process.wait()
