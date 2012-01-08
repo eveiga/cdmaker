0334ada69539db2f9417e83b21db0b8b8b4cc1e1
Edgar Veiga <edgar.veiga@ndrive.com>
2012-01-08 23:34:41 +0000

    Add carrier to server.py.

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 import subprocess
 
-def start_supplier(host, port):
-    pass
-
 def start_manufacturer(host, port):
     subprocess.Popen(['python', 'manufacturer/soap_views.py', host, port],)
 
 def start_backoffice(host, port):
     subprocess.Popen(['python', 'backoffice/soap_views.py', host, port],)
 
+def start_carrier(host, port):
+    subprocess.Popen(['python', 'carrier/soap_views.py', host, port],)
+
 if __name__=='__main__':
     try:
+        start_carrier("localhost","7885")
+        start_carrier("localhost","7886")
+        start_carrier("localhost","7887")
+        start_manufacturer("localhost","7888")
         start_manufacturer("localhost","7889")
         start_manufacturer("localhost","7890")
         start_backoffice("localhost","7891")
