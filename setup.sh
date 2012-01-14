ebbcc6a05ed8baa147f84114200a29f7d00eeac1
Edgar Veiga <edgar.veiga@ndrive.com>
2012-01-14 22:29:49 +0000

    Don't force python 2.7

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PYTHON_PATH=`which python2.7`
+PYTHON_PATH=`which python`
 if [ $PYTHON_PATH != "" ]
     then
         virtualenv -p $PYTHON_PATH --no-site-packages --distribute --prompt=[cdmaker-env] .env
         pip install -E .env -r requirements.txt
     else
-        echo "Can't find Python2.7 executable."
+        echo "Can't find Python executable."
 fi
