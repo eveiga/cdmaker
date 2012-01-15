0e317bd8628f2267e90bb937cf7e56b03bd3ceba
Edgar Veiga <edgar.veiga@ndrive.com>
2012-01-15 00:27:48 +0000

    Add virtualenv as native dependency.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 PYTHON_PATH=`which python`
 if [ $PYTHON_PATH != "" ]
     then
+        easy_install virtualenv
         virtualenv -p $PYTHON_PATH --no-site-packages --distribute --prompt=[cdmaker-env] .env
         pip install -E .env -r requirements.txt
     else
