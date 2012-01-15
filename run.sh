9b7c6ca2f7bac933dc22e61efa504026f38f1bf9
Edgar Veiga <edgar.veiga@ndrive.com>
2012-01-15 00:28:57 +0000

    Read execution.log while executing server.

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+LOGFILE=execution.log
+
+[[ -f $LOGFILE ]] | touch $LOGFILE
+
+( tail -f $LOGFILE & echo $! >&3 ) 3>pid
 
 source .env/bin/activate
-python server.py
+python server.py > /dev/null 2>&1
 
-cd -> /dev/null
+kill $(<pid)
